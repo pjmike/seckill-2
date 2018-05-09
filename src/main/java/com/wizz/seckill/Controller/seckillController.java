86255fa667033fa829f1e0a4371bfa370f04bd5c
joeylee <joeylee97@163.com>
2018-05-09 20:21:23 +0800

    update

--- src/main/java/com/wizz/seckill/Controller/seckillController.java
+++ src/main/java/com/wizz/seckill/Controller/seckillController.java
@@ -4,9 +4,12 @@ import com.wizz.seckill.Model.reqInfo;
 import com.wizz.seckill.Model.reqRes;
 import com.wizz.seckill.Mapper.reqInfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 class seckillController{
@@ -14,9 +17,12 @@ class seckillController{
     @Autowired
     reqInfoMapper infomap;
 
-    @RequestMapping(value = "/tickets" , method = RequestMethod.GET)
-    reqRes resHandler(){
-        //reqInfo rI = new reqInfo("李卓","15010180018","15092825097",0);
+    @RequestMapping(value = "/tickets" , method = RequestMethod.POST)
+    reqRes resHandler(@RequestBody Map<String, Object> params){
+        String phoneNum = (String)params.get("phoneNum");
+        String stuId = (String)params.get("stuId");
+        String stuName = (String)params.get("name");
+        //state 是IP查重 + 手机号查重通过
         infomap.updateInfo(0,"15092825097","15010180018","李卓");
         return new reqRes("true","");
     }
